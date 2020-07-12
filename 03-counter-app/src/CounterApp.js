7c2bb27490a4ddc895c1f3f8a6a959bf6722b205
María Sánchez Carvajal <msancarv@everis.com>
2020-07-12 17:12:27 +0200

    feat: handle reset and substract

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -4,12 +4,20 @@ import PropTypes from 'prop-types';
 const CounterApp = ({
     value
 }) => {
-    const [ counter, setCounter] = useState(0);
+    const [ counter, setCounter] = useState(value);
     console.log( counter, setCounter )
     // handleAdd
     const handleAdd = () => {
         // setCounter( counter + 1 );
         setCounter( (c) => c + 1 );
+    }
+    
+    const handleReset = () => {
+        setCounter( value );
+    }
+
+    const handleSubstract = () => {
+        setCounter( (c) => c - 1 );
     }
 
     return (
@@ -17,6 +25,8 @@ const CounterApp = ({
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
             <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
         </>
     );
 }
